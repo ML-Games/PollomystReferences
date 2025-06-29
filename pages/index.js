4a29d7f3c393182d7fb407102d38ebabea79bcf9
Maxence Lam <bcohtminc@gmail.com>
2025-06-29 21:08:20 +0100

    Updated reference list with new sources

--- pages/index.js
+++ pages/index.js
@@ -26,9 +26,7 @@ export default function Home() {
         <h1 style={{ fontSize: '2rem', marginBottom: '2rem' }}>References</h1>
         <ol>
           <li style={{ marginBottom: '1.5rem' }}>
-            Albano, L. et al. (2024) &apos;The Chemistry of Sporopollenin Ektexine and Endexine Layers Isolated
-            from Sunflower Pollen through an Ionic Liquid-Mediated Process,&apos; <em>ACS Omega</em>, 10(1),
-            pp. 411–421.{' '}
+            Albano, L. et al. (2024) &apos;The Chemistry of Sporopollenin Ektexine and Endexine Layers Isolated from Sunflower Pollen through an Ionic Liquid-Mediated Process,&apos; <em>ACS Omega</em>, 10(1), pp. 411–421.{' '}
             <a
               href="https://doi.org/10.1021/acsomega.4c06524"
               target="_blank"
@@ -36,6 +34,18 @@ export default function Home() {
               style={{ color: '#1a0dab', textDecoration: 'underline' }}
             >
               https://doi.org/10.1021/acsomega.4c06524
+            </a>
+          </li>
+
+          <li style={{ marginBottom: '1.5rem' }}>
+            Antihistamines (2022).{' '}
+            <a
+              href="https://patient.info/allergies-blood-immune/allergies/antihistamines"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: '#1a0dab', textDecoration: 'underline' }}
+            >
+              https://patient.info/allergies-blood-immune/allergies/antihistamines
             </a>
           </li>
 
@@ -52,8 +62,19 @@ export default function Home() {
           </li>
 
           <li style={{ marginBottom: '1.5rem' }}>
-            Grienenberger, E. and Quilichini, T.D. (2021) &apos;The Toughest Material in the Plant Kingdom: An
-            Update on Sporopollenin,&apos; <em>Frontiers in Plant Science</em>, 12.{' '}
+            Gross, T. (2024) Nasal sprays: How they work and how to use them correctly.{' '}
+            <a
+              href="https://www.covenanthealth.com/blog/nasal-sprays-how-they-work-and-how-to-use-them-correctly"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: '#1a0dab', textDecoration: 'underline' }}
+            >
+              https://www.covenanthealth.com/blog/nasal-sprays-how-they-work-and-how-to-use-them-correctly
+            </a>
+          </li>
+
+          <li style={{ marginBottom: '1.5rem' }}>
+            Grienenberger, E. and Quilichini, T.D. (2021) &apos;The Toughest Material in the Plant Kingdom: An Update on Sporopollenin,&apos; <em>Frontiers in Plant Science</em>, 12.{' '}
             <a
               href="https://doi.org/10.3389/fpls.2021.703864"
               target="_blank"
@@ -65,8 +86,31 @@ export default function Home() {
           </li>
 
           <li style={{ marginBottom: '1.5rem' }}>
-            Kim, S.-H., Park, H.-S. and Jang, J.-Y. (2011) &apos;Impact of meteorological variation on hospital
-            visits of patients with tree pollen allergy,&apos; <em>BMC Public Health</em>, 11(1).{' '}
+            Hay fever - Diagnosis and treatment - Mayo Clinic.{' '}
+            <a
+              href="https://www.mayoclinic.org/diseases-conditions/hay-fever/diagnosis-treatment/drc-20373045"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: '#1a0dab', textDecoration: 'underline' }}
+            >
+              https://www.mayoclinic.org/diseases-conditions/hay-fever/diagnosis-treatment/drc-20373045
+            </a>
+          </li>
+
+          <li style={{ marginBottom: '1.5rem' }}>
+            Immunotherapy - Allergy UK | National Charity (2024).{' '}
+            <a
+              href="https://www.allergyuk.org/resources/immunotherapy-factsheet/"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: '#1a0dab', textDecoration: 'underline' }}
+            >
+              https://www.allergyuk.org/resources/immunotherapy-factsheet/
+            </a>
+          </li>
+
+          <li style={{ marginBottom: '1.5rem' }}>
+            Kim, S.-H., Park, H.-S. and Jang, J.-Y. (2011) &apos;Impact of meteorological variation on hospital visits of patients with tree pollen allergy,&apos; <em>BMC Public Health</em>, 11(1).{' '}
             <a
               href="https://doi.org/10.1186/1471-2458-11-890"
               target="_blank"
@@ -102,8 +146,7 @@ export default function Home() {
           </li>
 
           <li style={{ marginBottom: '1.5rem' }}>
-            Singh, A.B. and Kumar, P. (2022) &apos;Climate change and allergic diseases: An overview,&apos;{' '}
-            <em>Frontiers in Allergy</em>, 3.{' '}
+            Singh, A.B. and Kumar, P. (2022) &apos;Climate change and allergic diseases: An overview,&apos; <em>Frontiers in Allergy</em>, 3.{' '}
             <a
               href="https://doi.org/10.3389/falgy.2022.964987"
               target="_blank"
@@ -115,8 +158,31 @@ export default function Home() {
           </li>
 
           <li style={{ marginBottom: '1.5rem' }}>
-            World Health Organisation (2024) &apos;Report of the WHO South-East Asia Regional meeting on
-            nutrition and climate change,&apos; <em>WHO Regional Office for South-East Asia</em>.{' '}
+            Steroid nasal sprays (2023).{' '}
+            <a
+              href="https://patient.info/treatment-medication/steroids/steroid-nasal-sprays"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: '#1a0dab', textDecoration: 'underline' }}
+            >
+              https://patient.info/treatment-medication/steroids/steroid-nasal-sprays
+            </a>
+          </li>
+
+          <li style={{ marginBottom: '1.5rem' }}>
+            Sublingual Immunotherapy (SLIT) | The Burghwood Clinic.{' '}
+            <a
+              href="https://www.burghwoodclinic.co.uk/clinical/treatments/sublingual-immunotherapy-slit"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: '#1a0dab', textDecoration: 'underline' }}
+            >
+              https://www.burghwoodclinic.co.uk/clinical/treatments/sublingual-immunotherapy-slit
+            </a>
+          </li>
+
+          <li style={{ marginBottom: '1.5rem' }}>
+            World Health Organisation (2024) &apos;Report of the WHO South-East Asia Regional meeting on nutrition and climate change,&apos; <em>WHO Regional Office for South-East Asia</em>.{' '}
             <a
               href="https://iris.who.int/bitstream/handle/10665/380407/SEA-NCD-112-eng.pdf"
               target="_blank"
@@ -128,9 +194,7 @@ export default function Home() {
           </li>
 
           <li style={{ marginBottom: '1.5rem' }}>
-            Zhang, Y. and Steiner, A.L. (2022) &apos;Projected climate-driven changes in pollen emission season
-            length and magnitude over the continental United States,&apos; <em>Nature Communications</em>,
-            13(1).{' '}
+            Zhang, Y. and Steiner, A.L. (2022) &apos;Projected climate-driven changes in pollen emission season length and magnitude over the continental United States,&apos; <em>Nature Communications</em>, 13(1).{' '}
             <a
               href="https://doi.org/10.1038/s41467-022-28764-0"
               target="_blank"
@@ -142,8 +206,7 @@ export default function Home() {
           </li>
 
           <li style={{ marginBottom: '1.5rem' }}>
-            Zhou, Y. and Dobritsa, A.A. (2025) &apos;Forging the pollen fortress: Cell biological mechanisms of
-            exine formation,&apos; <em>Current Opinion in Plant Biology</em>, 86, p. 102742.{' '}
+            Zhou, Y. and Dobritsa, A.A. (2025) &apos;Forging the pollen fortress: Cell biological mechanisms of exine formation,&apos; <em>Current Opinion in Plant Biology</em>, 86, p. 102742.{' '}
             <a
               href="https://doi.org/10.1016/j.pbi.2025.102742"
               target="_blank"
@@ -155,9 +218,7 @@ export default function Home() {
           </li>
 
           <li style={{ marginBottom: '1.5rem' }}>
-            Ziska, L.H. et al. (2019) &apos;Temperature-related changes in airborne allergenic pollen abundance
-            and seasonality across the northern hemisphere: a retrospective data analysis,&apos;{' '}
-            <em>The Lancet Planetary Health</em>, 3(3), pp. e124–e131.{' '}
+            Ziska, L.H. et al. (2019) &apos;Temperature-related changes in airborne allergenic pollen abundance and seasonality across the northern hemisphere: a retrospective data analysis,&apos; <em>The Lancet Planetary Health</em>, 3(3), pp. e124–e131.{' '}
             <a
               href="https://doi.org/10.1016/s2542-5196(19)30015-4"
               target="_blank"
